501809b5b5fd9eb15fa8360983e47f53b24320dd
iZeroKim <Kimgichuru6@gmail.com>
2020-09-01 16:29:13 +0300

    Added transitions without a scene object

---
+++ app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.sriyank.animationsdemo
+
+import android.os.Bundle
+import android.transition.Fade
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+	private var visibility = false
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+	}
+
+	fun fadeAnimation(view: View) {
+		val transition = Fade()
+		TransitionManager.beginDelayedTransition(sceneRoot, transition)
+		txvDescription.visibility = if (visibility) View.INVISIBLE else View.VISIBLE
+		visibility = !visibility
+	}
+
+	fun slideEffect(view: View) {
+
+		val transition = Slide()
+		TransitionManager.beginDelayedTransition(sceneRoot, transition)
+
+		txvDescription.visibility = if (visibility) View.INVISIBLE else View.VISIBLE
+		visibility = !visibility
+	}
+}
